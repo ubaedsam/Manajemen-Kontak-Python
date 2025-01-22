a59c6c1df8aa5016cf72e250cc5355c21e7705d3
Ubaed <ubaedasam@gmail.com>
2025-01-22 08:42:03 +0700

    Menggunakan file dan exception/error handling

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 # Progam Manajemen Kontak
 
+def membuka_kontak(path='kontak.txt'):
+    try:
+        with open(path, mode='r') as file:
+            kontak = file.readlines()
+    except FileNotFoundError:
+        kontak = [] # Jika terjadi error
+    return kontak
+
+
+def menyimpan_kontak(path='kontak.txt', isi=[]):
+    with open(path, mode='w') as file:
+        file.writelines(isi)
+
+
 class Kontak:
-    def __init__(self):
-        self.kontak = []
+    def __init__(self, path='kontak.txt'):
+        self.path = path
+        self.kontak = membuka_kontak(self.path)
 
     def melihat_kontak(self):
         # Melihat semua kontak
         if self.kontak:
             for num, item in enumerate(self.kontak, start=1):
-                print(f'\n{num}. {item["nama"]} ({item["HP"]}, {item["email"]})')
+                print(f'{num}. {item.strip()}')
         else:
             print("Kontak masih kosong!")
             return 1
@@ -18,7 +33,7 @@ class Kontak:
         nama = input("Masukkan nama kontak yang baru: ")
         HP = input("Masukkan nomor HP kontak yang baru: ")
         email = input("Masukkan email kontak yang baru: ")
-        kontak_baru = {'nama': nama, 'HP': HP, 'email': email}
+        kontak_baru = f'{nama} ({HP}, {email})\n'
         self.kontak.append(kontak_baru)
         print("Kontak baru berhasil ditambahkan!")
 
@@ -27,34 +42,48 @@ class Kontak:
         if self.melihat_kontak() == 1:
             return
         else:
-            i_hapus = int(input("Masukkan nomor kontak yang akan dihapus: "))
-            if 1 <= i_hapus <= len(self.kontak):
-                del self.kontak[i_hapus - 1]
-                print("Kontak yang dimaksud sudah dihapus")
-            else:
-                print("Nomor kontak tidak valid!")
+            try:
+                i_hapus = int(input("Masukkan nomor kontak yang akan dihapus: "))
+                if 1 <= i_hapus <= len(self.kontak):
+                    del self.kontak[i_hapus - 1]
+                    print("Kontak yang dimaksud sudah dihapus")
+                else:
+                    print("Nomor kontak tidak valid!")
+            except ValueError:
+                print("Input yang anda masukkan salah!")
 
     def mengubah_kontak(self):
         # Mengubah kontak
         if self.melihat_kontak() == 1:
             return
         else:
-            i_ubah = int(input("Masukkan nomor kontak yang akan diubah: "))
-            if 1 <= i_ubah <= len(self.kontak):
-                kontak_terpilih = self.kontak[i_ubah - 1]
-                print(f'Mengubah kontak: {kontak_terpilih["nama"]} ({kontak_terpilih["HP"]}, {kontak_terpilih["email"]})')
+            try:
+                i_ubah = int(input("Masukkan nomor kontak yang akan diubah: "))
+                if 1 <= i_ubah <= len(self.kontak):
+                    kontak_terpilih = self.kontak[i_ubah - 1].strip()
+                    print(f'Mengubah kontak: {kontak_terpilih}')
 
-                nama_baru = input(f"Masukkan nama baru (kosongkan jika tidak ingin mengubah): ") or kontak_terpilih["nama"]
-                HP_baru = input(f"Masukkan nomor HP baru (kosongkan jika tidak ingin mengubah): ") or kontak_terpilih["HP"]
-                email_baru = input(f"Masukkan email baru (kosongkan jika tidak ingin mengubah): ") or kontak_terpilih["email"]
+                    nama, sisa = kontak_terpilih.split(' (')
+                    HP, email = sisa[:-1].split(', ')
 
-                self.kontak[i_ubah - 1] = {'nama': nama_baru, 'HP': HP_baru, 'email': email_baru}
-                print("Kontak berhasil diubah!")
-            else:
-                print("Nomor kontak tidak valid!")
+                    nama_baru = input(f"Masukkan nama baru (kosongkan jika tidak ingin mengubah): ") or nama
+                    HP_baru = input(f"Masukkan nomor HP baru (kosongkan jika tidak ingin mengubah): ") or HP
+                    email_baru = input(f"Masukkan email baru (kosongkan jika tidak ingin mengubah): ") or email
 
-# Program utama (Tempat penyimpanan data / database)
-kontak_kantor = Kontak()
+                    self.kontak[i_ubah - 1] = f'{nama_baru} ({HP_baru}, {email_baru})\n'
+                    print("Kontak berhasil diubah!")
+                else:
+                    print("Nomor kontak tidak valid!")
+            except (ValueError, IndexError):
+                print("Input yang anda masukkan salah!")
+
+    def keluar_kontak(self):
+        # Menyimpan perubahan ke file
+        menyimpan_kontak(self.path, self.kontak)
+
+
+# Program utama
+kontak_kantor = Kontak() # Tempat penyimpanan data / database
 kontak_keluarga = Kontak()
 
 while True:
@@ -80,8 +109,8 @@ while True:
         kontak_keluarga.mengubah_kontak()
 
     elif pilihan == '5':
-        # Keluar dari kontak
-        print("Program selesai. Terima kasih!")
+        kontak_keluarga.keluar_kontak()
+        print("Perubahan telah disimpan. Program selesai.")
         break
 
     else:
